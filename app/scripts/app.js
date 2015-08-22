2c3fc2fd6bd49dd523343dba32ba31383337bbcd
yaskyj <yaskyj@yahoo.com>
2015-08-22 10:19:06 -0500

    refactor: Google maps not working, removed API key

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,7 +30,7 @@ angular.module('seaboardApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute'
   }])
   .config(['uiGmapGoogleMapApiProvider', function (GoogleMapApiProvider) {
     GoogleMapApiProvider.configure({
-      key: 'AIzaSyAfltuCM5AGdScNbvXCFPOgos-wpIeL6QA',
+      //key: 'AIzaSyAfltuCM5AGdScNbvXCFPOgos-wpIeL6QA',
       v: '3.17',
       libraries: 'weather,geometry,visualization'
     });
